df439f2e199391c156cbab98c0907f0cc33b4565
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2023-05-18 10:16:31 -0600

    Feat: Room class and methods

--- lib/room.rb
+++ lib/room.rb
@@ -1,6 +1,22 @@
 class Room
+attr_reader :category, :length, :width, :room
+  def initialize(category, length, width)
+    @room = room
+    @category = category
+    @length = length
+    @width = width
+    @is_painted = false
+  end
 
-  def initialize
+  def area
+   area = length * width.to_i
+  end
 
+  def is_painted? 
+    @is_painted
+  end
+
+  def paint
+    @is_painted = true
   end
 end
