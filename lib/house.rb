62d7e11fc1788eb0bcd5b4f710fb4abac186c6d9
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2023-05-18 10:18:47 -0600

    Feat: house and methods

---
+++ lib/house.rb
@@ -0,0 +1,35 @@
+class House 
+  attr_reader :address, :rooms
+  def initialize(price, address)
+    @price = price
+    @address = address 
+    @rooms = []
+  end
+
+  def price 
+    @price.delete("$").to_i
+  end
+
+  def add_room(room) 
+    @rooms << room
+  end
+
+  def above_market_average? 
+    if @price.to_i > 500000
+      true
+    else
+      false
+    end
+  end
+
+  def rooms_from_category(category) 
+    @rooms.select do |room|
+    room.category == category
+    end
+  end
+
+    def area
+      @room.length * @room.width.to_i
+    end
+
+end
